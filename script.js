e4e3963ba5f71f9798dfff72efc3b947509771f2
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-04-03 23:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function shortcut(s1, s2) {
-  // Convert both strings to lowercase
-  s1 = s1.toLowerCase();
-  s2 = s2.toLowerCase();
-  
-  // Extract the initial letters
-  const initialLetters = s1.charAt(0) + s2.charAt(0);
-  
-  return initialLetters.toUpperCase(); // Convert the result to uppercase
-}
-
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+cy.on("window:alert", str => {
+  expect(str.toLowerCase()).to.equal("ai");
+});
