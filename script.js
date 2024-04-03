e8ef2276b0801572b1af2511bb4f0309bfeb18b0
Sai Prasad <148376552+SaiPrasad-22@users.noreply.github.com>
2024-04-03 19:45:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function shortcut(s1, s2) {
-  if (s1.length === 0 || s2.length === 0) {
-    return '';
-  } else {
-    return s1[0].toUpperCase() + s2[0].toUpperCase();
-  }
+  // Convert both strings to lowercase
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+  
+  // Extract the initial letters
+  const initialLetters = s1.charAt(0) + s2.charAt(0);
+  
+  return initialLetters.toUpperCase(); // Convert the result to uppercase
 }
 
 // Do not change the code below.
